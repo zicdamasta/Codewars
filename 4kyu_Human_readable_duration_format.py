1de748d56d7eb208c7a2e6b595bae9446808f64a
alesmi <alesmi@ttu.ee>
2020-10-30 00:25:01 +0200

    Human readable duration format

--- 4kyu_Human_readable_duration_format.py
+++ 4kyu_Human_readable_duration_format.py
@@ -31,7 +31,8 @@
 # Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
 #
 # A unit of time must be used "as much as possible". 
-# It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+# It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, 
+# the duration specified by of a component must not be greater than any valid more significant unit of time.
 # ------------------------------------------------------
 
 def format_duration(seconds):
